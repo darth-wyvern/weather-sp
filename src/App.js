aaa86e6ceaccf09b59fc3bfae422aa3d7dc80bfd
darth-wyvern <nguyenquocnhan777@gmail.com>
2022-11-10 08:37:40 +0700

    add scss style

--- src/App.js
+++ src/App.js
@@ -6,45 +6,50 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="content" style={{ maxWidth: '1000px', margin: 'auto' }} >
-          <div className="title" style={{ display: 'flex', justifyContent: 'space-between', padding: '10px 30px' }}>
+        <div className="content">
+          <div className="title">
             <div>
-              <FontAwesomeIcon icon={'bars'} style={{ fontSize: '24pt' }} />
+              <FontAwesomeIcon icon={'bars'} className='icon-bars' />
             </div>
-            <div style={{ textAlign: 'center' }}>
+            <div>
               <div>MyENV</div>
-              <span style={{ marginRight: '.3rem' }}>Current Location</span>
-              <FontAwesomeIcon icon={'caret-down'} />
+              <span>Current Location <FontAwesomeIcon icon={'caret-down'} /></span>
             </div>
             <div>
-              <FontAwesomeIcon icon={'bell'} style={{ fontSize: '24pt' }} />
+              <FontAwesomeIcon icon={'bell'} className='icon-bell' />
             </div>
           </div>
-          <div className="detail" style={{ padding: '80px 0 100px', display: 'flex', gap: "1rem", alignItems: 'center', justifyContent: 'center' }}>
-            <div><FontAwesomeIcon icon={'cloud'} style={{ fontSize: '50pt', transform: 'rotateY(180deg)' }} /></div>
+          <div className="detail">
             <div>
-              <div style={{ fontSize: '25pt', fontWeight: '777' }}>Cloudy</div>
-              <div style={{ display: 'inline', fontWeight: 'bold', marginRight: '3rem', fontSize: '15pt' }}><FontAwesomeIcon icon={'temperature-three-quarters'} /> 29°C</div>
-              <div style={{ display: 'inline', fontWeight: 'bold', fontSize: '15pt' }}><FontAwesomeIcon icon={'droplet'} /> 27%</div>
+              <FontAwesomeIcon icon={'cloud'} className='icon-cloud' />
+            </div>
+            <div>
+              <div className="name">Cloudy</div>
+              <div className="temperature">
+                <FontAwesomeIcon icon={'temperature-three-quarters'} /> 29°C
+              </div>
+              <div className="droplet">
+                <FontAwesomeIcon icon={'droplet'} /> 27%
+              </div>
             </div>
           </div>
-          <div className="add" style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
-            <div style={{ maxWidth: '100px', width: '25%', backgroundImage: "linear-gradient(#70b9ff,#fff)", backgroundRepeat: 'no-repeat', backgroundSize: '1px 100%', backgroundPositionX: 'right', padding: '10px 0' }}>
+          <div className="add">
+            <div className="item break">
               <div>PSI</div>
-              <div style={{ background: '#1ae863', borderRadius: '5px', fontSize: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 'auto', padding: '0 5px', width: '40px', height: '36px' }}>23</div>
+              <div className="number-psi">23</div>
               <div>Good</div>
             </div>
-            <div style={{ maxWidth: '100px', width: '25%', backgroundImage: "linear-gradient(#70b9ff,#fff)", backgroundRepeat: 'no-repeat', backgroundSize: '1px 100%', backgroundPositionX: 'right', padding: '10px 0' }}>
+            <div className="item break">
               <div>RAIN</div>
-              <div style={{ padding: '0 5px', margin: '5px' }}>0</div>
+              <div className="number-rain">0</div>
               <div>mm</div>
             </div>
-            <div style={{ maxWidth: '100px', width: '25%', backgroundImage: "linear-gradient(#70b9ff,#fff)", backgroundRepeat: 'no-repeat', backgroundSize: '1px 100%', backgroundPositionX: 'right', padding: '10px 0' }}>
+            <div className="item break">
               <div >DENGUE</div>
-              <div style={{ width: '50px', height: '50px', border: '1px solid white', borderRadius: '50%', margin: '8px auto auto' }}></div>
+              <div className="number-dengue"></div>
             </div>
-            <div style={{ maxWidth: '100px', width: '25%', padding: '10px 0', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-              <FontAwesomeIcon icon='plus' style={{ fontSize: '25pt' }} />
+            <div className="item center-height">
+              <FontAwesomeIcon icon='plus' className="icon-plus" />
               <div>Add</div>
             </div>
           </div>
