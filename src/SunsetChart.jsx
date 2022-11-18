6cc639d9b3d70ecf47161f565c2eddc193f88cea
darth-wyvern <nguyenquocnhan777@gmail.com>
2022-11-18 17:06:53 +0700

    smooth animation sunset-chart

--- src/SunsetChart.jsx
+++ src/SunsetChart.jsx
@@ -3,7 +3,11 @@ import Sketch from "react-p5";
 
 function SunsetChart() {
   const myDiv = useRef();
+  const moon = useRef();
+  const sun = useRef();
   const chart = [
+    { time: 0, value: 0 },
+    { time: 0, value: 0 },
     { time: 0, value: 50 },
     { time: 400, value: 15 },
     { time: 800, value: 80 },
@@ -20,22 +24,19 @@ function SunsetChart() {
     { time: 5200, value: 50 },
     { time: 5600, value: 90 },
     { time: 6000, value: 120 },
+    { time: 6400, value: 0 },
+    { time: 6400, value: 0 },
   ];
 
-  var chartResponsive = [];
+  let chartResponsive = [];
 
-  const setup = (p5, canvasParentRef) => {
-    var _width = canvasParentRef.offsetWidth;
-    p5.createCanvas(_width * 6, 280).parent(canvasParentRef);
-    for (let i = 0; i < chart.length; i++) {
-      let t = (chart[i].time * myDiv.current.offsetWidth) / 1000;
-      chartResponsive.push({ time: t, value: chart[i].value });
-    }
-  };
+  let hour = 0;
+  let minute = 0;
+  let h = 140;
+  let w = 6400;
+  let sessionDay = "am";
 
-  const pixel = (p5, x, y, color) => {
-    p5.noStroke();
-    p5.fill(color);
+  const pixel = (p5, x, y) => {
     p5.rect(x, y, 1, 1);
   };
 
@@ -47,87 +48,96 @@ function SunsetChart() {
     return output;
   };
 
-  const draw = (p5) => {
-    p5.background(255);
-    let hour = 0;
-    let minute = 0;
-    let responseChart = myDiv.current.offsetWidth / 1000;
-    let start = (425 - 15) * responseChart;
-    let h = 140;
-    let w = 6400;
-    let sessionDay = "am";
-    let range = myDiv.current.offsetWidth;
-    let cur = myDiv.current.scrollLeft + range / 2;
-    let s = p5.PI / range;
-    let sunX = cur;
-    let sunY = 240 + p5.sin((start + cur) * s) * h;
-    let timeCalc = (start + cur) / (range / 12 / 60) + 425 + 12 * 60;
+  let widthDiv = 0;
+  let start = 0;
+  let responsive = 1;
 
-    p5.beginShape();
-    p5.curveVertex(0 * responseChart, 240 - 0);
-    p5.curveVertex(0 * responseChart, 240 - 0);
-
-    for (let i = 0; i < chartResponsive.length; i++) {
-      p5.curveVertex(chartResponsive[i].time, 240 - chartResponsive[i].value);
-    }
-
-    for (let i = 0; i < chartResponsive.length; i++) {
-      p5.text(
-        chartResponsive[i].value / 100 + " m",
-        chartResponsive[i].time,
-        240 - chartResponsive[i].value - 10
-      );
-    }
-
-    p5.fill("#70b9ff");
-    p5.curveVertex(6400 * responseChart, 240 - 0);
-    p5.curveVertex(6400 * responseChart, 240 - 0);
-    p5.endShape();
-
-    p5.textSize(12);
-    for (let i = start; i < start + w; i++) {
-      pixel(p5, i - start, 240 + p5.sin(i * s) * h, p5.color(255, 100, 0));
-    }
-
-    p5.rect(sunX - 5, sunY - 5, 10, 10);
-
-    hour = p5.round(((timeCalc - 30) / 60) % 24);
-    minute = leftPad(p5.round(timeCalc % 60), 2);
+  const numberToTime = (cur) => {
+    let timeCalc = (start + cur) / (widthDiv / 12 / 60) + 425 + 12 * 60;
+    hour = Math.round(((timeCalc - 30) / 60) % 24);
+    minute = leftPad(Math.round(timeCalc % 60), 2);
     if (hour > 12) {
       sessionDay = "pm";
       hour = hour % 12;
     }
-    if (hour === 0) {
-      hour = 12;
+    hour = hour === 0 ? 12 : hour;
+
+    return hour + " : " + minute + " " + sessionDay;
+  };
+
+  const setup = (p5, canvasParentRef) => {
+    p5.pixelDensity(1);
+    p5.noStroke();
+    widthDiv = canvasParentRef.offsetWidth;
+    responsive = widthDiv / 1000;
+    start = (425 - 15) * responsive;
+
+    // convert tide chart data
+    p5.createCanvas(widthDiv * 6, 280).parent(canvasParentRef);
+    for (let i = 0; i < chart.length; i++) {
+      let t = p5.round((chart[i].time * myDiv.current.offsetWidth) / 1000);
+      chartResponsive.push({ time: t, value: chart[i].value });
     }
 
-    p5.fill(p5.color(0, 0, 0, 100));
+    // Draw tide
+    p5.fill("#70b9ff");
+    p5.beginShape();
+    chartResponsive.forEach((item) => {
+      p5.curveVertex(item.time, 240 - item.value);
+    });
+    p5.endShape();
 
+    // Draw night
+    p5.fill("#0004");
     for (let i = 0; i < 5; i++) {
-      p5.rect(
-        -320 * responseChart + i * 2000 * responseChart,
-        0,
-        800 * responseChart,
-        240
-      );
+      let calcDay = -320 * responsive + i * 2000 * responsive;
+      p5.rect(calcDay, 0, 800 * responsive, 240);
     }
 
+    // Write tide
+    p5.fill("darkcyan");
+    chartResponsive.forEach((item) => {
+      p5.text(item.value / 100 + " m", item.time, 240 - item.value - 10);
+    });
+
+    // Draw sunset graph
+    p5.fill(p5.color(255, 100, 0));
+    p5.textSize(12);
+    for (let i = start; i < start + w; i++) {
+      pixel(p5, i - start, 240 + p5.sin((i * Math.PI) / widthDiv) * h);
+    }
+  };
+
+  const draw = (p5) => {
+    let cur = myDiv.current.scrollLeft + widthDiv / 2;
+    let sunX = cur;
+    let sunY = 240 + Math.sin(((start + cur) * Math.PI) / widthDiv) * h;
+
+    // draw sun
+    sun.current.style.display = "block";
+    sun.current.style.top = sunY - 295 + "px";
+    if (sunY - 295 > -55) {
+      sun.current.style.display = "none";
+    }
+
+    // Draw moon
+    moon.current.style.display = "none";
     for (let i = 0; i < 5; i++) {
-      if (
-        sunX > -320 * responseChart + i * 2000 * responseChart &&
-        sunX <
-          -320 * responseChart + i * 2000 * responseChart + 800 * responseChart
-      ) {
-        p5.rect(sunX - 11, 100, 20, 20);
+      let calcIsDay = -320 * responsive + i * 2000 * responsive;
+
+      if (sunX > calcIsDay && sunX < calcIsDay + 800 * responsive) {
+        moon.current.style.display = "block";
       }
     }
 
+    // Box time
     p5.fill("#bbbfc3").textSize(16);
     p5.rect(0, 240, p5.width, 280);
 
+    // Write time
     p5.fill("black");
     p5.textAlign(p5.CENTER, p5.CENTER);
-    p5.text(hour + " : " + minute + " " + sessionDay, cur, 260);
+    p5.text(numberToTime(cur), cur, 260);
   };
 
   return (
@@ -140,6 +150,7 @@ function SunsetChart() {
           maxWidth: "1000px",
           margin: "0 auto",
           overflow: "auto",
+          width: "100%",
         }}
       >
         <Sketch setup={setup} draw={draw} className="p5" />
@@ -161,6 +172,29 @@ function SunsetChart() {
               marginBottom: "45px",
               left: "calc(50% - 1px)",
             }}
+          />
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/196/196685.png"
+            alt=""
+            style={{
+              position: "absolute",
+              width: "20px",
+              height: "20px",
+              top: "-200px",
+              left: "calc(50% - 10px)",
+            }}
+            ref={moon}
+          />
+          <img
+            src="https://i.pinimg.com/originals/35/8b/f6/358bf617c093163955378bec6b0f492e.png"
+            alt=""
+            style={{
+              position: "absolute",
+              width: "20px",
+              height: "20px",
+              left: "calc(50% - 10px)",
+            }}
+            ref={sun}
           />
         </div>
       </div>
--- src/loadIcon.js
+++ src/loadIcon.js
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee, faBars, faBell, faCaretDown, faCloud, faTemperatureThreeQuarters, faDroplet, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faBars, faBell, faCaretDown, faCloud, faTemperatureThreeQuarters, faDroplet, faPlus, faSun } from '@fortawesome/free-solid-svg-icons'
 
 export default function loadIcon() {
-  library.add(faCoffee, faBars, faBell, faCaretDown, faCloud, faTemperatureThreeQuarters, faDroplet, faPlus);
+  library.add(faCoffee, faBars, faBell, faCaretDown, faCloud, faTemperatureThreeQuarters, faDroplet, faPlus, faSun);
 }
